a61f86f20fe06ca95c43c4e4c20ce7c480097851
Andrew Shearer <ashearer@sevone.com>
2017-02-15 15:22:39 -0500

    Set name when creating dagger

--- src/main/java/WeaponFactory.java
+++ src/main/java/WeaponFactory.java
@@ -4,6 +4,7 @@
 public class WeaponFactory {
     public Weapon buildDagger() {
         Weapon dagger = new Weapon();
+        dagger.setName("Dagger");
         dagger.setAccuracy(75);
         dagger.setDamage(10);
         dagger.setRange(1);
